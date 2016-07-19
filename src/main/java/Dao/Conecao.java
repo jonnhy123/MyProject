a85119e51aedbaf591ac1deb87542d42514c910a
Jhonatan Mattana <jhonatan-mattana@hotmail.com>
2016-07-18 21:15:46 -0300

    Criando metodo fecharConecao

--- src/main/java/Dao/Conecao.java
+++ src/main/java/Dao/Conecao.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Conecao {
-
+	private static Connection conn;
 	private static final String USUARIO = "root";
     private static final String SENHA = "4221";
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/LotoFacil";
@@ -17,9 +17,17 @@ public class Conecao {
         // Registrar o driver
         Class.forName(DRIVER);
         // Capturar a conexão
-        Connection conn = DriverManager.getConnection(URL, USUARIO, SENHA);
+        conn = DriverManager.getConnection(URL, USUARIO, SENHA);
         // Retorna a conexao aberta
         return conn;
     }
 
+    public void fecharConecao(){
+    	 try{
+    	  conn.close();
+    	  System.out.println("Conexao finalizada com sucesso");
+    	 }catch(Exception e){
+    	  System.out.println("Erro ao fechar conexao com banco" + e.getMessage());
+    	 }
+    }
 }
