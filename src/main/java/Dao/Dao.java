fa1695e7374a481840eac4386ed4fe90a20d62be
Jhonatan Mattana <jhonatan-mattana@hotmail.com>
2016-07-18 21:04:18 -0300

    Criando metodo abrirConecção

--- src/main/java/Dao/Dao.java
+++ src/main/java/Dao/Dao.java
@@ -1,5 +1,25 @@
 package Dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Dao {
+
+	private static final String USUARIO = "root";
+    private static final String SENHA = "4221";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/LotoFacil";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+
+    // Conectar ao banco
+    public static Connection abrir() throws Exception {
+        // Registrar o driver
+        Class.forName(DRIVER);
+        // Capturar a conexão
+        Connection conn = DriverManager.getConnection(URL, USUARIO, SENHA);
+        // Retorna a conexao aberta
+        return conn;
+    }
 
 }
